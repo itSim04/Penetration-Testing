5e079be052d1b0406db4ae5d6529134af20d45f4
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-03-21 23:08:20 +0200

    Started handling Directory search

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, urllib, re
+import sys, urllib, re, requests
 
 master = sys.argv[1]
 
@@ -24,4 +24,24 @@ def extractLinks(url, links):
     links.write("\n\n")
 
 with open("./links.bat", 'w') as links_output:
+
     extractLinks(master, links_output)
+
+    with open("./dirs_dictionary.bat", "r") as dirs:
+
+        for line in dirs.readlines()[0:15]:
+                                                                
+            code = requests.get(master + line.rstrip('\n')).status_code
+
+            if code == 404:
+
+                print('Directory/File does not exist: ' + line.rstrip('\n'))
+            
+            elif code // 100 == 2 or code == 403:
+
+                print('Directory/File does exist: ' + line.rstrip('\n') + " " + str(code)) 
+
+       
+
+       
+            
