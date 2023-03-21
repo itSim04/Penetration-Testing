f34c21b1623490acac5de4faa47c9d0f9ce39bec
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-03-21 19:59:58 +0200

    Applied the regex on the previously obtained URL

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, urllib
+import sys, urllib, re
 
 master = sys.argv[1]
 
@@ -6,4 +6,7 @@ req = urllib.request.Request(master)
 webUrl = urllib.request.urlopen(req)
 print(webUrl)
                                     
-link_pattern = r'href="[^#\"]{2,}?"'                                    
+link_pattern = r'href="[^#\"]{2,}?"'
+
+refs = re.findall(link_pattern, webUrl.read().decode())
+print(refs)
