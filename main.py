c7fc8024be2439702bb81f5578dc4c68c3730e72
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-03-21 19:58:43 +0200

    Designed a Regular Expression to extract links from the URL

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ req = urllib.request.Request(master)
 webUrl = urllib.request.urlopen(req)
 print(webUrl)
                                     
-                                    
+link_pattern = r'href="[^#\"]{2,}?"'                                    
